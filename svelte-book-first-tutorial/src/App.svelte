5799c12dccc95962ba0bb113974aeb6f7aee44c2
kjmatu <kenji.matsunaga@me.com>
2023-10-29 11:10:01 +0900

    split slider component

--- svelte-book-first-tutorial/src/App.svelte
+++ svelte-book-first-tutorial/src/App.svelte
@@ -1,60 +1,42 @@
 <script>
-let product = {
-  id: 'svelte-book',
-  name: 'Svelte Guide',
-  price: 3500,
+  import Slider from './Slider.svelte';
 
-  images: [
-    "https://github.com/svelte-book/sample-app/raw/main/static/svelte-book-1.png",
-    "https://github.com/svelte-book/sample-app/raw/main/static/svelte-book-2.png",
-    "https://github.com/svelte-book/sample-app/raw/main/static/svelte-book-3.png",
-  ],
-}
+  let product = {
+    id: 'svelte-book',
+    name: 'Svelte Guide',
+    price: 3500,
+
+    images: [
+      "https://github.com/svelte-book/sample-app/raw/main/static/svelte-book-1.png",
+      "https://github.com/svelte-book/sample-app/raw/main/static/svelte-book-2.png",
+      "https://github.com/svelte-book/sample-app/raw/main/static/svelte-book-3.png",
+    ],
+  }
 
 
-let relatedProducts = [
-  {
-    id: 'react-book',
-    name: 'React Book',
-    price: 3500,
-  },
-  {
-    id: 'vue-book',
-    name: 'Vue Book',
-    price: 3500,
-  },
-  {
-    id: 'angular-book',
-    name: 'Angular Book',
-    price: 3500,
-  },
-]
+  let relatedProducts = [
+    {
+      id: 'react-book',
+      name: 'React Book',
+      price: 3500,
+    },
+    {
+      id: 'vue-book',
+      name: 'Vue Book',
+      price: 3500,
+    },
+    {
+      id: 'angular-book',
+      name: 'Angular Book',
+      price: 3500,
+    },
+  ]
 
   let cart = [];
 
   function addToCart(productId) {
     cart = [...cart, productId];
   }
-
-
-let sliderCenterIndex = 0;
-let sliderLeftIndex  = product.images.length - 1;
-let sliderRightIndex = 1;
-
-function sliderMoveLeft() {
-  const length = product.images.length;
-  sliderCenterIndex = (sliderCenterIndex - 1 + length) % length;
-  sliderLeftIndex   = (sliderCenterIndex - 1 + length) % length;
-  sliderRightIndex  = (sliderCenterIndex + 1) % length;
-}
-
-function sliderMoveRight(params) {
-  const length = product.images.length;
-  sliderCenterIndex = (sliderCenterIndex + 1) % length;
-  sliderLeftIndex   = (sliderCenterIndex - 1 + length) % length;
-  sliderRightIndex  = (sliderCenterIndex + 1) % length;
-}
-
 
 </script>
 
@@ -73,13 +55,7 @@ function sliderMoveRight(params) {
 <article class="product">
   <div class="product-main">
     <div class="image-container">
-      <div class="slider">
-        <img src={product.images[sliderLeftIndex]} alt="スライダー画像 左" class="slider-item left">
-        <img src={product.images[sliderCenterIndex]} alt="スライダー画像" class="slider-item">
-        <img src={product.images[sliderRightIndex]} alt="スライダー画像 右" class="slider-item right">
-        <button class="slider-left-button" on:click={sliderMoveLeft}> ← </button>
-        <button class="slider-right-button" on:click={sliderMoveRight}> → </button>
-      </div>
+      <Slider images={product.images} />
     </div>
 
     <div>
@@ -163,44 +139,6 @@ function sliderMoveRight(params) {
     width: 100%;
     max-width: 400px;
     overflow: hidden;
-  }
-
-  .image-container img {
-    width: 100%;
-  }
-
-  .slider {
-    position: relative;
-    width: 80%;
-    margin: 0 10%;
-  }
-
-  .slider-item {
-    width: 100%;
-  }
-
-  .slider-item.left {
-    position: absolute;
-    top: 0;
-    right: 100%;
-  }
-
-  .slider-item.right {
-    position: absolute;
-    top: 0;
-    left: 100%;
-  }
-
-  .slider-left-button {
-    position: absolute;
-    top: 50%;
-    right: 100%;
-  }
-
-  .slider-right-button {
-    position: absolute;
-    top: 50%;
-    left: 100%;
   }
 
 </style>
